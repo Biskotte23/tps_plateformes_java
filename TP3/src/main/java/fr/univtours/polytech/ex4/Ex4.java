49f4efb585602b89b32f2f99e69812b84d2d6b4a
Charles <54975624+Chalumos@users.noreply.github.com>
2023-05-22 14:37:46 +0200

    TP3 ex4

---
+++ TP3/src/main/java/fr/univtours/polytech/ex4/Ex4.java
@@ -0,0 +1,58 @@
+package fr.univtours.polytech.ex4;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ex4
+ */
+@WebServlet("/Ex4")
+public class Ex4 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("reponse", "");
+		request.getRequestDispatcher("ex4.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  // Création de la liste
+        List<Integer> listeNombre = new ArrayList<>();
+        
+        // Ajout des nombres de 1 à 10 à la liste
+        for (int i = 1; i <= 10; i++) {
+        	listeNombre.add(i);
+        }
+        
+        // Tirage d'un nombre aléatoire
+        Random random = new Random();
+        int index = random.nextInt(listeNombre.size());
+        int nombreAleatoire = listeNombre.get(index);
+        
+        int nombreChoisi = Integer.parseInt(request.getParameter("nombreChoisi"));
+        
+        String reponse  = "";
+        if(nombreAleatoire == nombreChoisi) {
+        	reponse = "Victoire :) ";
+        }
+        else if(nombreChoisi > 10 || nombreChoisi < 0) {
+        	reponse = "Nombre choisi incorrect ! ";
+        }
+        else {
+        	reponse = "Perdu :( ";
+        }
+        
+        
+        request.setAttribute("reponse", reponse);
+        request.getRequestDispatcher("ex4.jsp").forward(request, response);
+	}
+
+}
---
+++ TP3/src/main/webapp/ex4.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Exercice 4</title>
+</head>
+<body>
+	<form action="Ex4" method="post">
+		<input type="number" name="nombreChoisi"> <input type="submit"
+			value="valider">
+	</form>
+	<h1>${requestScope.reponse}</h1>
+</body>
+</html>
