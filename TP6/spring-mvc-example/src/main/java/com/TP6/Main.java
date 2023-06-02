a29112eb64d0c5b53dac6af58d00bef03db99054
Charles <54975624+Chalumos@users.noreply.github.com>
2023-06-02 15:38:29 +0200

    TP6

---
+++ TP6/spring-mvc-example/src/main/java/com/TP6/Main.java
@@ -0,0 +1,15 @@
+package com.TP6;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+	private static final Logger logger = LogManager.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		logger.info("Hello log4j !");
+		logger.debug("Hello log4j !");
+	}
+
+}
